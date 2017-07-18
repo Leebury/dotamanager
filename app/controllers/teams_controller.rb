6d1e9ed3ce7453dfd4cae2909f02a91c8d236107
Leebury <lbury@spartaglobal.com>
2017-07-18 13:48:21 +0100

    Corrected team new redirect

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,9 +1,11 @@
 class TeamsController < ApplicationController
 
 	def index
+		@teams = Team.all
 	end
 
 	def show
+		@team = Team.find(params[:id])
 	end
 
 	def edit
@@ -17,13 +19,22 @@ class TeamsController < ApplicationController
 	end
 
 	def create
+		puts params
 		@team = Team.new(team_params)
 		@team.save
 		redirect_to @team
 	end
 
 	def destroy
-end
+	end
+
+
+	private
+	    # Never trust parameters from the scary internet, only allow the white list through.
+	    def team_params
+			params.require(:team).permit(:team_name, :player_names, :wins, :losses)
+	    end
+
 
 
 end
